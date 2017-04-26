ae74b7e55a33515b670f9448f1562a99547e0b60
Abhay Varshney <varshneyabhay@gmail.com>
2017-04-26 02:10:58 -0700

    implementing icon files

--- android/app/src/main/java/com/vibe/MainApplication.java
+++ android/app/src/main/java/com/vibe/MainApplication.java
@@ -3,6 +3,8 @@ package com.vibe;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.smixx.reactnativeicons.ReactNativeIcons;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReactNativeIcons(),
             new FBSDKPackage()
       );
     }
